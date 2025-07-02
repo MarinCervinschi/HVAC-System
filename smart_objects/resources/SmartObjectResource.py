60f7bef520efb347197f85a15ac62c41c2c644fe
MarinCervinschi <marincervinschi3@gmail.com>
2025-07-02 17:33:08 +0200

    ci: SmartObjectResource

--- smart_objects/resources/SmartObjectResource.py
+++ smart_objects/resources/SmartObjectResource.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import Generic, TypeVar, List
+from .ResourceDataListener import ResourceDataListener
 import json
 
 T = TypeVar("T")
@@ -7,11 +8,34 @@ T = TypeVar("T")
 
 class SmartObjectResource(ABC, Generic[T]):
     def __init__(self, resource_id: str):
-        self.resource_id = resource_id
+        self.resource_id: str = resource_id
+        self.resource_listener_list: List[ResourceDataListener] = []
 
     @abstractmethod
     def load_updated_value(self) -> T:
         pass
+
+    def add_data_listener(self, resource_data_listener: ResourceDataListener) -> None:
+        """Add a new listener to be notified of changes"""
+        if resource_data_listener not in self.resource_listener_list:
+            self.resource_listener_list.append(resource_data_listener)
+
+    def remove_data_listener(
+        self, resource_data_listener: ResourceDataListener
+    ) -> None:
+        """Remove an existing listener"""
+        if resource_data_listener in self.resource_listener_list:
+            self.resource_listener_list.remove(resource_data_listener)
+
+    def notify_update(self, updated_value: T) -> None:
+        """Notify all registered listeners of a value change"""
+        if not self.resource_listener_list:
+            print("No active listeners - nothing to notify")
+            return
+
+        for listener in self.resource_listener_list:
+            if listener is not None:
+                listener.on_data_changed(self, updated_value)
 
     def to_json(self):
         return json.dumps(self, default=lambda o: o.__dict__)
