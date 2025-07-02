4c83dfde89b5f8b254315a054e643fd66baa9a8a
isacapp <isa.cappellino@gmail.com>
2025-07-02 17:51:24 +0200

    feat: telemetry messages

---
+++ messages/telemetry_message.py
@@ -0,0 +1,28 @@
+from dataclasses import asdict
+import time
+import json
+from typing import Any, Dict
+from messages.GenericMessage import GenericMessage
+
+
+class TelemetryMessage(GenericMessage):
+
+    def __init__(
+        self,
+        type_: str,
+        data_value: Any,
+        timestamp: int = None,
+        metadata: Dict[str, Any] = None,
+    ):
+        if not type_:
+            raise ValueError("Type must be a non-empty string.")
+
+        super().__init__(type_, metadata)
+        self.data_value = data_value
+        self.timestamp = timestamp if timestamp is not None else int(time.time() * 1000)
+
+    def to_dict(self) -> Dict[str, Any]:
+        return asdict(self)
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__)
