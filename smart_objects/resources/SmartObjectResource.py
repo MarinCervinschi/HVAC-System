d78b7d396f629309f19b3851d32f7e5a8ab14f52
MarinCervinschi <marincervinschi3@gmail.com>
2025-07-03 21:32:19 +0200

    ci: use logging in SmartObjectResourse

--- smart_objects/resources/SmartObjectResource.py
+++ smart_objects/resources/SmartObjectResource.py
@@ -2,9 +2,9 @@ from abc import ABC, abstractmethod
 from typing import Generic, TypeVar, List
 from .ResourceDataListener import ResourceDataListener
 import json
+import logging
 
 T = TypeVar("T")
-
 
 class SmartObjectResource(ABC, Generic[T]):
     def __init__(self, resource_id: str):
@@ -12,6 +12,8 @@ class SmartObjectResource(ABC, Generic[T]):
         self.data_type: T = None
         self.resource_id: str = resource_id
         self.resource_listener_list: List[ResourceDataListener[T]] = []
+
+        self.logger = logging.getLogger(f"{__name__}.{resource_id}")
 
     @abstractmethod
     def load_updated_value(self) -> T:
@@ -21,6 +23,8 @@ class SmartObjectResource(ABC, Generic[T]):
         """Add a new listener to be notified of changes"""
         if resource_data_listener not in self.resource_listener_list:
             self.resource_listener_list.append(resource_data_listener)
+        else:
+            self.logger.debug(f"Listener already registered: {resource_data_listener}")
 
     def remove_data_listener(
         self, resource_data_listener: ResourceDataListener[T]
@@ -28,11 +32,13 @@ class SmartObjectResource(ABC, Generic[T]):
         """Remove an existing listener"""
         if resource_data_listener in self.resource_listener_list:
             self.resource_listener_list.remove(resource_data_listener)
+        else:
+            self.logger.debug(f"Listener not found: {resource_data_listener}")
 
     def notify_update(self, updated_value: T) -> None:
         """Notify all registered listeners of a value change"""
         if not self.resource_listener_list:
-            print("No active listeners - nothing to notify")
+            self.logger.info("No active listeners - nothing to notify")
             return
 
         for listener in self.resource_listener_list:
