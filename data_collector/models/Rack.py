6bf5369ed15aeabd309e57fef92e8c932cc5e323
MarinCervinschi <marincervinschi3@gmail.com>
2025-07-04 21:24:27 +0200

    ci: Rack

--- data_collector/models/Rack.py
+++ data_collector/models/Rack.py
@@ -1,16 +1,13 @@
 from abc import ABC, abstractmethod
 from typing import Dict
 from smart_objects.devices.SmartObject import SmartObject
-from smart_objects.devices.rack_cooling_unit import RackCoolingUnit
 
 
 class Rack(ABC):
-    def __init__(self, rack_id: str, rack_type: str, cooling_unit: RackCoolingUnit):
+    def __init__(self, rack_id: str, rack_type: str):
         self.rack_id = rack_id
         self.rack_type = rack_type
         self.smart_objects: Dict[str, SmartObject] = {}
-
-        self.add_smart_object(cooling_unit)
 
     def add_smart_object(self, smart_object: SmartObject):
         self.smart_objects[smart_object.object_id] = smart_object
