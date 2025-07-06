d44b930dd19583955aab687dfb734d61106cb263
MarinCervinschi <marincervinschi3@gmail.com>
2025-07-06 21:40:15 +0200

    ci: coap config

--- config/coap_conf_params.py
+++ config/coap_conf_params.py
@@ -5,4 +5,4 @@ class CoapConfigurationParameters(object):
     COAP_SERVER_ADDRESS: ClassVar[str] = "127.0.0.1"
     COAP_SERVER_PORT: ClassVar[int] = 5683
 
-    BASIC_URI: ClassVar[str] = "coap://{host}:{port}/hvac/room"
+    BASIC_URI: ClassVar[str] = f"coap://{COAP_SERVER_ADDRESS}:{COAP_SERVER_PORT}"
