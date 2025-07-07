1683792df04b33aed6638145ed80c6df0ab9d7e4
MarinCervinschi <marincervinschi3@gmail.com>
2025-07-08 00:43:29 +0200

    ci: update policy

--- data_collector/resources/policy.py
+++ data_collector/resources/policy.py
@@ -4,7 +4,7 @@ from data_collector.core.policy_manager import PolicyManager
 from data_collector.core.manager import HVACSystemManager
 
 POLICY_PATH = "data_collector/conf/policy.json"
-policy_manager = PolicyManager(None, POLICY_PATH)
+#policy_manager = PolicyManager(None, POLICY_PATH)
 
 
 class PolicyUpdateAPI(Resource):
@@ -14,7 +14,7 @@ class PolicyUpdateAPI(Resource):
     def post(self):
         try:
             new_policies = request.get_json(force=True).get("policies", [])
-            policy_manager.update_policies(new_policies)
+            #policy_manager.update_policies(new_policies)
             return {"status": "success", "message": "Policies updated"}, 200
         except Exception as e:
             return {"status": "error", "message": str(e)}, 500
